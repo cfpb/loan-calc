1a0dfdc0f8291a7ee337410bee4121d741c61818
Adam Scott <adam.scott@cfpb.gov>
2014-04-09 16:55:48 -0400

    simplify naming

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var MonthlyRateCalculator = function(principal, interestRate, months) {
+var MonthlyRateCalc = function(principal, interestRate, months) {
   'use strict';
 
   var monthlyInterest = (interestRate/100)/12;
@@ -6,4 +6,4 @@ var MonthlyRateCalculator = function(principal, interestRate, months) {
   return principal * (monthlyInterest / (1 - Math.pow(1 + monthlyInterest, -(months))));
 };
 
-module.exports = MonthlyRateCalculator;
+module.exports = MonthlyRateCalc;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-var MonthlyRateCalculator = require('../index.js');
+var MonthlyRateCalc = require('../index.js');
 
 exports['calculate'] = function (test) {
-    test.equal(MonthlyRateCalculator(100000, 6, 360), 599.550525152759);
-    console.log(MonthlyRateCalculator(100000, 6, 360));
-    test.done();
+  test.equal(MonthlyRateCalc(100000, 6, 360), 599.550525152759);
+  console.log(MonthlyRateCalc(100000, 6, 360));
+  test.done();
 };
