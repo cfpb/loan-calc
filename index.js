7b628c426981e79eb1dd2030a83e93fbd92ee60f
Adam Scott <adam.scott@cfpb.gov>
2014-04-09 17:09:20 -0400

    do simple two decimal place formatting

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var MonthlyRateCalc = function(principal, interestRate, months) {
   'use strict';
 
-  var monthlyInterest = (interestRate/100)/12;
+  var monthlyInterest = (interestRate/100)/12,
+      calculate;
 
-  return principal * (monthlyInterest / (1 - Math.pow(1 + monthlyInterest, -(months))));
+  calculate = principal * (monthlyInterest / (1 - Math.pow(1 + monthlyInterest, -(months))));
+
+  return roundValue(calculate);
+
+  function roundValue(num) {
+    return num.toFixed(2);
+  }
 };
 
 module.exports = MonthlyRateCalc;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var MonthlyRateCalc = require('../index.js');
 
 exports['calculate'] = function (test) {
-  test.equal(MonthlyRateCalc(100000, 6, 360), 599.550525152759);
+  test.equal(MonthlyRateCalc(100000, 6, 360), 599.55);
   console.log(MonthlyRateCalc(100000, 6, 360));
   test.done();
 };
