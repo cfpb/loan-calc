9971f1d2f6d036a4c9ffb14445a558e72761d56f
Adam Scott <adam.scott@cfpb.gov>
2014-04-10 08:46:49 -0400

    more loan value tests

--- test/test.js
+++ test/test.js
@@ -1,11 +1,23 @@
 var LoanCalc = require('../index.js');
 
+// test that we're calculating the correct monthly payment
 exports['A 30 year, $100,000 loan with a 6% interest rate should be repaid at $599.55 per month'] = function (test) {
   test.equal(LoanCalc.paymentCalc(100000, 6, 360), 599.55);
   test.done();
 };
 
+exports['A 15 year, $200,000 loan with a 5% interest rate should be repaid at $1,581.59 per month'] = function (test) {
+  test.equal(LoanCalc.paymentCalc(200000, 5, 180), 1581.59);
+  test.done();
+};
+
+// test that we're calculating the correct total interest paid
 exports['A 30 year, $200,000 loan with a 5% interest rate should have a total interest of $186,511.57'] = function (test) {
   test.equal(LoanCalc.totalInterest(200000, 5, 360), 186511.57);
+  test.done();
+};
+
+exports['A 40 year, $200,000 loan with a 5% interest rate should have a total interest of $262,908.74'] = function (test) {
+  test.equal(LoanCalc.totalInterest(200000, 5, 480), 262908.74);
   test.done();
 };
